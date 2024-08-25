dd9a281d527b8084b519c30fedbbb3d7876d746e
shockpast <shockpast@gmail.com>
2024-08-25 15:20:19 +0300

    fix: batch was not working properly

--- install.bat
+++ install.bat
@@ -1,24 +1,31 @@
 @echo off
 
-set /p "SOSU_MIDDLEWARE=Did you install Bun? (Y/N): "
+goto SOSU_START
 
-if /i "%SOSU_MIDDLEWARE%"=="Y" goto MIDDLEWARE_BUN
-if /i "%SOSU_MIDDLEWARE%"=="N" goto MIDDLEWARE_BATCH
+:SOSU_START
+  set /p "SOSU_MIDDLEWARE=Did you install Bun? (Y/N): "
 
-echo "Y" or "N" was expected as input, aborting.
-goto COMMON_EXIT
+  if /i "%SOSU_MIDDLEWARE%"=="Y" goto MIDDLEWARE_BUN
+  if /i "%SOSU_MIDDLEWARE%"=="N" goto MIDDLEWARE_BATCH
+
+  echo "Y" or "N" was expected as input, aborting.
+  goto COMMON_EXIT
 
 :MIDDLEWARE_BUN
-bun install > nul 2>&1
-reg add "HKCR\sosu\shell\open\command" /ve /d "%cd%/scripts/middleware-bun.bat %cd% %%1" /f
+  bun install > nul 2>&1
 
-echo Installed "bun" version.
-goto COMMON_EXIT
+  reg add "HKCR\sosu" /v "URL Protocol" /f > nul 2>&1
+  reg add "HKCR\sosu\shell\open\command" /ve /d "%cd%/scripts/middleware-bun.bat %cd% %%1" /f > nul 2>&1
+
+  echo Installed "bun" version.
+  goto COMMON_EXIT
 
 :MIDDLEWARE_BATCH
-%windir%\regedit.exe /s "scripts/postinstall-batch.reg"
-echo Installed "batch-only" version.
-goto COMMON_EXIT
+  reg add "HKCR\sosu" /v "URL Protocol" /f > nul 2>&1
+  reg add "HKCR\sosu\shell\open\command" /ve /d "%cd%/scripts/middleware-batch.bat %%1" /f > nul 2>&1
+
+  echo Installed "batch-only" version.
+  goto COMMON_EXIT
 
 :COMMON_EXIT
-exit
+  exit
--- scripts/middleware-batch.bat
+++ scripts/middleware-batch.bat
@@ -7,5 +7,5 @@ set SOSU_COMMAND=%1
 set SOSU_COMMAND=%SOSU_COMMAND:~7%
 set SOSU_COMMAND=%SOSU_COMMAND:~0,-1%
 
-curl -sS -L --output "%SOSU_COMMAND%.osz" --output-dir "%0\..\songs" "https://direct.osuokayu.moe/api/v1/d/%SOSU_COMMAND%"
-%SOSU_PATH% "%0\..\songs\%SOSU_COMMAND%.osz"
+curl -sS -L --output "%SOSU_COMMAND%.osz" --output-dir "%0\..\..\songs" "https://direct.osuokayu.moe/api/v1/d/%SOSU_COMMAND%"
+%SOSU_PATH% "%0\..\..\songs\%SOSU_COMMAND%.osz"
